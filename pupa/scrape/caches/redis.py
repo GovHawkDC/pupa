562e38b3a798201d6b953d33dca59aad117d9c51
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-04-16 00:20:22 -0600

    port over the redis getter method

--- pupa/scrape/caches/redis.py
+++ pupa/scrape/caches/redis.py
@@ -13,7 +13,24 @@ class Redis(Cache):
         self.conn = redis.StrictRedis(host=host, port=port, password=password)
 
     def get(self, key, **kwargs):
-        pass
+        # NOTE: The redis client returns UTF-8 encoded strings, so this is a
+        # workaround to convert the stored value, if present and an encoded
+        # string, to just a plain old string for comparison
+        #
+        # E.g., `b'bar' == 'bar' # False`
+        #
+        # For not using `decode_responses` keyword arg in connection, @see:
+        # https://stackoverflow.com/questions/25745053\
+        #     /about-char-b-prefix-in-python3-4-1-client-connect-to-redis\
+        #     #comment40254650_25745110
+        value = self.conn.get(key)
+        if value is None:
+            return value
+        # @see https://stackoverflow.com/a/34870210/1858091
+        try:
+            return value.decode('utf-8')
+        except Exception:
+            return value
 
     def set(self, key, value, **kwargs):
         pass
