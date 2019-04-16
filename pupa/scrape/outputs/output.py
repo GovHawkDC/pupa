5e7b8d2f2688bbee5dfb36c666e64c9f770cd6b5
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-04-16 01:07:49 -0600

    integrate cache logic into base outputter class

--- pupa/scrape/outputs/output.py
+++ pupa/scrape/outputs/output.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from datetime import datetime, timezone
 
 from pupa import utils
+import pupa.scrape.objects.objects as obj_helpers
 
 
 class Output(metaclass=ABCMeta):
@@ -32,18 +33,37 @@ class Output(metaclass=ABCMeta):
     def handle_output(self, obj):
         pass
 
-    # TODO: Extra arg for `cache`
-    def save_object(self, obj):
+    def pre_handle_output(self, obj, **kwargs):
+        cache_target = kwargs.get('cache_target')
+
+        # Handle as normal if no cache target specified
+        if cache_target is None:
+            self.handle_output(obj)
+            return
+
+        obj_attrs = obj_helpers.get_obj_attrs(obj)
+
+        # Check for object key
+        if obj_attrs.get('key') is None:
+            self.scraper.info('no cache key found for %s', obj)
+            return
+
+        cached_obj_hash = cache_target.get(obj_attrs.get('key'))
+        obj_hash = obj_helpers.get_obj_hash(obj)
+
+        # Bail if object already processed and cached
+        if cached_obj_hash and obj_hash == cached_obj_hash:
+            self.scraper.info('%s already cached', obj)
+            return
+
+        # Cache and handle as normal
+        cache_target.set(obj_attrs.get('key'), obj_hash)
+        self.handle_output(obj)
+
+    def save_object(self, obj, **kwargs):
         obj.pre_save(self.scraper.jurisdiction.jurisdiction_id)
 
-        # actual output handling, to be handled by subclass
-        # TODO: Idea, `cache` is passed in as arg
-        #
-        # TODO: if !cache.exists(obj):
-        #           cache.set(obj)
-        #           then self.handle_output
-        #
-        self.handle_output(obj)
+        self.pre_handle_output(obj, **kwargs)
 
         # validate after writing, allows for inspection on failure
         try:
