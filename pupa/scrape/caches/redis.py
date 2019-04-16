4eda471f54ea490b3f002e6c344c3377e77e0e2b
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-04-16 00:18:09 -0600

    port over redis constructor for establishing connection

--- pupa/scrape/caches/redis.py
+++ pupa/scrape/caches/redis.py
@@ -7,7 +7,10 @@ from pupa.scrape.caches.cache import Cache
 class Redis(Cache):
 
     def __init__(self, **kwargs):
-        pass
+        host = os.environ.get('REDIS_HOST')
+        port = os.environ.get('REDIS_PORT')
+        password = os.environ.get('REDIS_PASSWORD')
+        self.conn = redis.StrictRedis(host=host, port=port, password=password)
 
     def get(self, key, **kwargs):
         pass
