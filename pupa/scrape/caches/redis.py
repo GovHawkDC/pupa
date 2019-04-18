205c2b1338624b4c16b7cdc6b80f286b2904b166
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-04-18 00:23:03 -0600

    simplify decoding of bytes to utf-8 using built-in kwarg for redis constructor

--- pupa/scrape/caches/redis.py
+++ pupa/scrape/caches/redis.py
@@ -10,27 +10,11 @@ class Redis(Cache):
         host = os.environ.get('REDIS_HOST')
         port = os.environ.get('REDIS_PORT')
         password = os.environ.get('REDIS_PASSWORD')
-        self.conn = redis.Redis(host=host, port=port, password=password)
+        self.conn = redis.Redis(host=host, port=port, password=password
+                                decode_responses=True)
 
     def get(self, key, **kwargs):
-        # NOTE: The redis client returns UTF-8 encoded strings, so this is a
-        # workaround to convert the stored value, if present and an encoded
-        # string, to just a plain old string for comparison
-        #
-        # E.g., `b'bar' == 'bar' # False`
-        #
-        # For not using `decode_responses` keyword arg in connection, @see:
-        # https://stackoverflow.com/questions/25745053\
-        #     /about-char-b-prefix-in-python3-4-1-client-connect-to-redis\
-        #     #comment40254650_25745110
-        value = self.conn.get(key)
-        if value is None:
-            return value
-        # @see https://stackoverflow.com/a/34870210/1858091
-        try:
-            return value.decode('utf-8')
-        except Exception:
-            return value
+        return self.conn.get(key)
 
     def set(self, key, value, **kwargs):
         return self.conn.set(key, value)
