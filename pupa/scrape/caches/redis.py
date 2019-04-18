ead059035c61b2e492dd0133d1f6548c8b486659
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-04-18 00:55:32 -0600

    try out connection cache in redis

--- pupa/scrape/caches/redis.py
+++ pupa/scrape/caches/redis.py
@@ -2,16 +2,23 @@ import os
 import redis
 
 from pupa.scrape.caches.cache import Cache
+from pupa.scrape.connections.manager import get_conn, set_conn
 
 
 class Redis(Cache):
 
     def __init__(self, **kwargs):
-        host = os.environ.get('REDIS_HOST')
-        port = os.environ.get('REDIS_PORT')
-        password = os.environ.get('REDIS_PASSWORD')
-        self.conn = redis.Redis(host=host, port=port, password=password
-                                decode_responses=True)
+        c = get_conn('REDIS')
+
+        if c is None:
+            host = os.environ.get('REDIS_HOST')
+            port = os.environ.get('REDIS_PORT')
+            password = os.environ.get('REDIS_PASSWORD')
+            c = redis.Redis(host=host, port=port, password=password
+                            decode_responses=True)
+            set_conn('REDIS', c)
+
+        self.conn = c
 
     def get(self, key, **kwargs):
         return self.conn.get(key)
