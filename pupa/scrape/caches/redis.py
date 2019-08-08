49c5cb055f2e378b07801c3a9c5df3dbc8d1c2fd
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-08-08 11:27:34 -0700

    update redis cache class to use singleton metaclass and also ditch the connection manager stuff for now

--- pupa/scrape/caches/redis.py
+++ pupa/scrape/caches/redis.py
@@ -2,22 +2,20 @@ import os
 import redis
 
 from pupa.scrape.caches.cache import Cache
-from pupa.scrape.connections.manager import get_conn, set_conn
+from pupa.scrape.connections.singleton import Singleton
 
 
-class Redis(Cache):
+class Redis(Cache, metaclass=Singleton):
 
-    def __init__(self, **kwargs):
-        c = get_conn('REDIS')
+    def __init__(self, scraper, **kwargs):
+        super().__init__(scraper)
 
-        if c is None:
-            host = os.environ.get('REDIS_HOST')
-            port = os.environ.get('REDIS_PORT')
-            password = os.environ.get('REDIS_PASSWORD')
-            c = redis.Redis(host=host, port=port, password=password, decode_responses=True)
-            set_conn('REDIS', c)
+        host = os.environ.get('REDIS_HOST')
+        port = os.environ.get('REDIS_PORT')
+        password = os.environ.get('REDIS_PASSWORD')
+        self.conn = redis.Redis(host=host, port=port, password=password, decode_responses=True)
 
-        self.conn = c
+        self.scraper.info('cache checking enabled with redis as target')
 
     def get(self, key, **kwargs):
         return self.conn.get(key)
